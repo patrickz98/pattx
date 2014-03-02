9302f51b8de2db479d24cfd4097057915f30c795
patrickzyolo <patrick@kapp-mm.de>
2014-03-02 15:25:55 +0100

    new php

---
+++ apache-head.php
@@ -0,0 +1,31 @@
+<?php
+
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+exec("/usr/bin/php /home/odroid/php/apache.php");
+
+?>
