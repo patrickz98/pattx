0c646d065a583881e2fb42323cd2ad123f73daa7
patrickzyolo <patrick@kapp-mm.de>
2014-03-02 15:13:42 +0100

    Add ripper.sh

--- boom.php
+++ boom.php
@@ -1,9 +1,7 @@
 <?php
 
 exec("/usr/bin/php /home/odroid/php/apache.php 1");
-echo "1 \n";
 exec("/usr/bin/php /home/odroid/php/apache.php 2");
-echo "2 \n";
 exec("/usr/bin/php /home/odroid/php/apache.php 3");
 exec("/usr/bin/php /home/odroid/php/apache.php 4");
 exec("/usr/bin/php /home/odroid/php/apache.php 5");
