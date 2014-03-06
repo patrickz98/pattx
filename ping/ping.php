5f66ad16403e3f63896a54aacff8c9d7ac8b6518
paddy <paddy@debian>
2014-03-06 14:18:33 +0100

    new logs files

--- ping/ping.php
+++ ping/ping.php
@@ -8,13 +8,18 @@ $ip = "192.168.0.1";
 
 $online = exec("ping $ip -c 1 |grep received");
 
+$time = date("H:i");
+$date = date("d-m-Y");
+
+#$file-on =/on"
+#$file-off
+
 if ($online == "1 packets transmitted, 1 received, 0% packet loss, time 0ms")
 	{
 	echo "$ip ist online \n";
 
-	$date = date("d.m.Y H:i");
-        $inhalt = "$ip ist online um $date";
-        $handle = fopen ("log.txt", "a");
+        $inhalt = "$ip ist online um $time";
+        $handle = fopen ("on/$date.txt", "a");
         fwrite ($handle," ".$inhalt."\r\n");
         fclose ($handle);
 
