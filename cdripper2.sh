37af6c512f59b18086ba4466a8c22c82979fdef8
paddy <paddy@debian>
2014-03-10 18:59:15 +0100

    new ripper.sh with scp mv

--- cdripper2.sh
+++ cdripper2.sh
@@ -1,3 +1,12 @@
+
 #!/bin/sh
 #/usr/bin/mplayer -dvd-device /dev/sr0 dvdnav://$1 -dumpstream -dumpfile ./ripped_$1.vob
 /usr/local/bin/mplayer -dvd-device /dev/sr0 dvdnav://$1 -dumpstream -dumpfile ./ripped_$1.vob
+
+echo "scp ripped_$1.vob odroid@odroid-X2.local:/media/iPtarick/DVD"
+
+scp ripped_$1.vob odroid@odroid-X2.local:/media/iPtarick/DVD
+
+echo "rm ripped_$1.vob"
+
+rm ripped_$1.vob
