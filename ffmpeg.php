21b457c85eee78010fe3217540409094a10d2090
dezi <dezi@kappa-mm.de>
2014-04-08 20:51:55 +0200

    finish ffmpeg.php

--- ffmpeg.php
+++ ffmpeg.php
@@ -1,21 +1,41 @@
 #!/usr/bin/php
 <?php
 
+
 $id = "http://odroid-u3.local";
 $jobs = "/jobs/";
 $a = 1;
 $b = ".txt";
 $c = exec("curl '$id'/xxx.txt| wc -l");
+$d = 0;
 $ziel = "cache/";
 $out = "out";
 $format = ".mp4";
-$done = exec("curl '$id'/done.txt| grep media");
+
+exec("curl '$id'/done.txt| grep media > done.txt");
+$done = file("done.txt");
+$donewc = exec("cat done.txt| wc -l");
+
 
 while($a < $c)
 {
+
 	$dat = exec("curl $id$jobs$a$b| grep media");
 
+	while($d < $donewc)
+	{
+		if($done[$d] = $dat)
+		{
+			echo $done[$d];
+			$a++;
+			return;
+		}
+		$d++;
+	}
+
+
 	echo "ffmpeg -i $id$dat $ziel$out$a$format\n";
+	//exec("ffmpeg -i $id$dat $ziel$out$a$format");
 	$a++;
 }
 
