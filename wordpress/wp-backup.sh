178d47b133f9b3cf601ad85af743d01708ac7c6a
dezi <patrick@kappa-mm.de>
2014-06-18 16:46:53 +0200

    enter kimz wp

--- wordpress/wp-backup.sh
+++ wordpress/wp-backup.sh
@@ -26,4 +26,15 @@ mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $DB_FILE
 tar cvzf $Name $File $DB_FILE
 rm $DB_FILE
 
+Name="kimz-backup.tgz"
+File="/var/www/kimz"
+
+DB_USER="kim"
+DB_PASS="hallo6"
+DB_NAME="kimwpdb"
+DB_FILE="kimwpdb.$NOW.sql"
+
+mysqldump -u$DB_USER -p$DB_PASS $DB_NAME > $DB_FILE
+tar cvzf $Name $File $DB_FILE
+rm $DB_FILE
 
