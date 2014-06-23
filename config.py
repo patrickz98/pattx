00576bcd045235c3182ce2bab595556cd67ef951
dezi <patrick@kappa-mm.de>
2014-06-23 22:02:06 +0200

    check in

---
+++ config.py
@@ -0,0 +1,1 @@
+hosts = ["odroid-u4.local"]
--- ffmpeg.py
+++ ffmpeg.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python
+
 import subprocess
 import sys
 import os
+from config import *
 
+pwd = os.getcwd()
 slaves = 2
+
 #file = "../thrones-4x03-copy.ts"
-file = "test.ts"
+#file = "test.ts"
 #file = "../Recordings/Jerry-Cotton/Jerry-Cotton.mkv"
+file = "see.ts"
 
 #Video length
 def getLength(file):
@@ -23,6 +29,7 @@ def getLength(file):
 		print "video error"
 	
 #Video cutter
+videoparts = []
 def cut(file):
 	length = getLength(file)
 	h = int(length[:2])
@@ -31,11 +38,10 @@ def cut(file):
 	seconds = (int(h) * 60  + int(m)) * 60 + float(s)
 	
 	print "seconds in video: " + str(seconds)
-	#print seconds / slaves
 	
 	a = 0
 	b = 0
-	while (slaves >= a):
+	while (slaves > a):
 		c = seconds / slaves
 		b = b + c
 		d = b - c
@@ -44,14 +50,24 @@ def cut(file):
 		
 		out = "out-" + str(a) + ".ts"
 		os.popen("ffmpeg -i %s -acodec copy -vcodec copy -scodec copy -ss %s -t %s %s 1>/dev/null 2>/dev/null" % (str(file), str(d), str(b), out)).readlines()
+		
+		videoparts.append(out)
 
-#ffmpeg -i "concat:out-1.ts|out-2.ts|out-3.ts" -vcodec copy -acodec copy  end.ts
-#ffmpeg -i test.ts -acodec copy -vcodec copy -scodec copy -ss 00:00:00 -t 00:10:23.06 cut.ts	
+#link the videos 
+def build():
+	bulid = "concat:"
+	for i in videoparts:
+		bulid += i + "|"
+	os.popen('ffmpeg -i "%s" -vcodec copy -acodec copy  end.ts 1>/dev/null 2>/dev/null' % (bulid)).readlines()
 
 #ssh command transfer
-def ssh(COMMAND):
-	HOST="odroid-u4.local"
-	#COMMAND="uname -a"
+def sshhead():
+	for h in hosts:
+		ssh(h)
+
+def ssh(HOST):
+	#HOST = "odroid-u4.local"
+	COMMAND = "ffmpeg -i http://odroid-u3.local/out-2.ts out.mp4; scp out.mp4 odroid@odroid-u3.local:%s; rm out.mp4" % pwd
 
 	ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
 	                       shell=False,
@@ -60,14 +76,22 @@ def ssh(COMMAND):
 	result = ssh.stdout.readlines()
 	if result == []:
 	    error = ssh.stderr.readlines()
-	    print >>sys.stderr, "ERROR: %s" % error
+	    #print >>sys.stderr, "ERROR: %s" % error
 	    return "ssh error" 
 	else:
 	    return  str(result)[2:-4]
 
+#Creat symlinks
+def symlinks():
+	for i in videoparts:
+		print "make symlink from %s to /var/www/odroid/%s" % ( i, i)
+		os.popen('ln -s %s/%s /var/www/odroid/%s' % ( pwd, i, i)).readlines()
+	
 
-#print ssh("uname -a")
-#print ssh("uptime")
-#print ssh("uptime")
-print getLength(file)
-cut(file)
+
+#print getLength(file)
+#cut(file)
+#symlinks()
+sshhead()
+#print "build"
+#build()
