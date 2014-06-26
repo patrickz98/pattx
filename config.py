1ded7487a94054eed39f3fb6fdb5dc1626acfaa5
dezi <patrick@kappa-mm.de>
2014-06-26 16:58:35 +0200

    check in

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-hosts = ["odroid-u4.local"]
+hosts = ["odroid@odroid-u4.local", "patty@debian.local", "patty@debian.local"]
+#file = "thrones-4x04.ts"
+file = "see.ts"
--- ffmpeg.py
+++ ffmpeg.py
@@ -3,15 +3,15 @@
 import subprocess
 import sys
 import os
+import time
 from config import *
 
 pwd = os.getcwd()
-slaves = 2
+slaves = len(hosts)
 
-#file = "../thrones-4x03-copy.ts"
+#file = "thrones-4x04.ts"
 #file = "test.ts"
 #file = "../Recordings/Jerry-Cotton/Jerry-Cotton.mkv"
-file = "see.ts"
 
 #Video length
 def getLength(file):
@@ -29,8 +29,8 @@ def getLength(file):
 		print "video error"
 	
 #Video cutter
-videoparts = []
-def cut(file):
+videoparts = {}
+def getvideoparts(file):
 	length = getLength(file)
 	h = int(length[:2])
 	m = length[3:(len(length) - 6)]
@@ -41,57 +41,79 @@ def cut(file):
 	
 	a = 0
 	b = 0
-	while (slaves > a):
+	while (slaves >= a):
 		c = seconds / slaves
 		b = b + c
 		d = b - c
 		print ("cut from %d to %d" % (d, b))
+		
+		out = "part-" + str(a)
+		#os.popen("ffmpeg -i %s -acodec copy -vcodec copy -scodec copy -ss %s -t %s %s 1>/dev/null 2>/dev/null" % (str(file), str(d), str(b), out)).readlines()
+		#videoparts.append(out)
+		videoparts.update({out:[int(d),int(b)]})
+		print out
+		print videoparts[out]
 		a = a + 1
-		
-		out = "out-" + str(a) + ".ts"
-		os.popen("ffmpeg -i %s -acodec copy -vcodec copy -scodec copy -ss %s -t %s %s 1>/dev/null 2>/dev/null" % (str(file), str(d), str(b), out)).readlines()
-		
-		videoparts.append(out)
-
-#link the videos 
-def build():
-	bulid = "concat:"
-	for i in videoparts:
-		bulid += i + "|"
-	os.popen('ffmpeg -i "%s" -vcodec copy -acodec copy  end.ts 1>/dev/null 2>/dev/null' % (bulid)).readlines()
 
 #ssh command transfer
 def sshhead():
+	a = 0
 	for h in hosts:
-		ssh(h)
+		part = "part-%s" % str(a)
+		#part = a
+		ssh(h, part)
+		a += 1
 
-def ssh(HOST):
+buildparts = []
+def ssh(HOST, part):
+	global buildparts
 	#HOST = "odroid-u4.local"
-	COMMAND = "ffmpeg -i http://odroid-u3.local/out-2.ts out.mp4; scp out.mp4 odroid@odroid-u3.local:%s; rm out.mp4" % pwd
-
-	ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
+	part1 = videoparts[part][0]
+	part2 = videoparts[part][1]
+	out = part + ".mp4"
+	print "%s" % out
+	buildparts.append(out)
+	
+	#main ssh 
+	COMMAND = "nohup ffmpeg -i http://odroid-u3.local/%s -ss %d -t %d %s 1>/dev/null 2>/dev/null && scp ~/%s odroid@odroid-u3.local:%s 1>/dev/null 2>/dev/null && rm ~/%s" % (file, part1, part2, out, out, pwd, out)
+	subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
 	                       shell=False,
 	                       stdout=subprocess.PIPE,
 	                       stderr=subprocess.PIPE)
-	result = ssh.stdout.readlines()
-	if result == []:
-	    error = ssh.stderr.readlines()
-	    #print >>sys.stderr, "ERROR: %s" % error
-	    return "ssh error" 
-	else:
-	    return  str(result)[2:-4]
 
 #Creat symlinks
-def symlinks():
-	for i in videoparts:
-		print "make symlink from %s to /var/www/odroid/%s" % ( i, i)
-		os.popen('ln -s %s/%s /var/www/odroid/%s' % ( pwd, i, i)).readlines()
+def symlink(file):
+	print "make symlink from %s to /var/www/odroid/%s" % (file, file)
+	os.popen('rm /var/www/odroid/%s' % file).readlines()
+	os.popen('ln -s %s/%s /var/www/odroid/%s' % ( pwd, file, file)).readlines()
 	
-
-
-#print getLength(file)
-#cut(file)
-#symlinks()
+#link the videos 
+def build():
+	bulid = "concat:"
+	finish = []
+	while(True):
+		objects = os.popen('ls').readlines()
+		for a in objects:
+			for b in buildparts:
+				if a[:-1] == b:
+					print a[:-1] + " is finish"
+					finish.append(a)
+				
+		print "len(finish) " + str(len(finish))
+		print str(slaves)
+		if (len(finish) == slaves):
+			for i in finish:
+				bulid += i + "|"
+			os.popen('ffmpeg -i "%s" -vcodec copy -acodec copy end.mp4 1>/dev/null 2>/dev/null' % (bulid)).readlines()
+			break
+		else:
+			print "Wait of hosts"
+			time.sleep(15)
+		
+	
+print getLength(file)
+getvideoparts(file)
+symlink(file)
 sshhead()
-#print "build"
-#build()
+print "build"
+build()
