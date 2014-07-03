1b409238dccee6992b8597ab60b1804440016965
patrickzyolo <patrick@kappa-mm.de>
2014-07-03 17:13:41 +0200

    check in

---
+++ python/crypto.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys
+import os
+
+
+input = sys.argv
+input.remove(input[0])
+
+file = ""
+ofile = ""
+
+for i in input:
+	if i == "-i":
+		file += input[input.index(i) + 1]
+	if i == "-o":
+		ofile += input[input.index(i) + 1]
+
+def encrypt():
+	os.popen("openssl aes-256-cbc -a -salt -in %s -out %s.crypt" % ( file, file)).readlines()
+
+def decrypt():
+	os.popen("openssl aes-256-cbc -d -a -in %s -out %s" % ( ofile, (ofile[:-6]))).readlines()
+
+if ofile == "":
+	encrypt()
+else:
+	decrypt()
