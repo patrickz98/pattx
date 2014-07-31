5ae72ed8fd29f56d36611c163ff8e06becb7a775
patrickzyolo <patrick@kappa-mm.de>
2014-07-31 23:11:15 +0200

    check in

--- garrysmod-stargate-mod.sh
+++ garrysmod-stargate-mod.sh
@@ -5,3 +5,10 @@ git clone https://github.com/RafaelDeJongh/cap.git; \
 git clone https://github.com/RafaelDeJongh/cap_resources.git; \
 
 git clone https://github.com/wiremod/wire.git; \
+
+git clone https://github.com/RafaelDeJongh/cap_fonts; \
+
+git clone https://github.com/SnakeSVx/spacebuild.git; \
+
+git clone https://github.com/SnakeSVx/sbep.git; \
+
