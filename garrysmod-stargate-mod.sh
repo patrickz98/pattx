eec21cb743cabdb6653ddc9035b4ac243ae67458
dezi <patrick@kappa-mm.de>
2014-08-05 22:36:32 +0200

    Check in

--- garrysmod-stargate-mod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd $HOME/Library/Application\ Support/Steam/SteamApps/common/GarrysMod/garrysmod/addons/; \
-git clone https://github.com/RafaelDeJongh/cap.git; \
-git clone https://github.com/RafaelDeJongh/cap_resources.git; \
-git clone https://github.com/wiremod/wire.git; \
-git clone https://github.com/RafaelDeJongh/cap_fonts; \
-git clone https://github.com/SnakeSVx/spacebuild.git; \
