f16e750ebdfa95e7472882eda76844f928239dbe
patrickzyolo <patrick@kappa-mm.de>
2014-08-17 12:37:14 +0200

    check in

--- spon/analytics.py
+++ spon/analytics.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
@@ -8,9 +9,42 @@ os.popen("python zeit.py").readlines()
 os.popen("python welt.py").readlines()
 
 text = ["news-spon.txt", "news-welt.txt", "news-zeit.txt"]
+
+def find(word):
+	for txt in text:
+		tx = open(txt, "r").readlines()
+		count = 0
+		for a in tx:
+			if word in a:
+#				print a[:-1]
+				count = count + 1
+		return count
+
+bad = ["um", "an", "von", "in", "den", 
+       "im", "die", "mit", "bei", "sich", 
+       "des", "Die", "der", "ab", "am",
+       "-", "und", "auf", "zum", "Der", 
+       "vor", ":", "ist", "zum", "eine", "zu", "aus", 
+       "ein", "er", "ich", "so", "es", "ter", "zur", 
+       "Ja", "in", "Ab", "Im", "dem", "ja", "wer", "du", "ein",
+       "er", "war", "Er", "tun", "als", "hin", "los", "In",
+       "Ein", "wird", "So", "La", "wir", "Eine", "Das", "Wir",
+       "nun", "sind", "ins", "nun", "auch", "+++", "+++:",
+       "nicht", "noch", "ohne", "seit", "letzte", "neu", 
+       "Diese", "diese", "Auf", "Wo", "Sie", "immer", "einer",
+       "Neue", "of"]
+
+list = {}
 for txt in text:
 	tx = open(txt, "r").readlines()
-	for a in tx:
-		if "US" in a:
-			print a[:-1]
-	
+        
+	for b in tx:
+		b = b
+		for c in b.split():
+			if c not in bad:
+				if find(c) > 2:
+#					print c + ":  " + str(find(c))
+					list.update({c:find(c)})
+
+for l in list:
+	print str(l) + ": " + str(list[l])
