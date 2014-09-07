95215da5bd2deb0a195afaecbdefd43748ba1fba
patrickzyolo <patrick@kappa-mm.de>
2014-09-07 19:17:59 +0200

    check in

--- spon/data.py
+++ spon/data.py
@@ -8,33 +8,34 @@ def main():
 
 	nown = []
 	raw = []
+	for txt in files:
+		if ".data" in txt:
+			pass
+		else:
+			files.remove(txt)
+	
 	for a in files:
 		a = dir + "/" + a
-		if ".data" in a:
-			file = open(a, "r")
+		file = open(a, "r")
 
-			for b in file:
-				name = b[:b.find(":")]
-				zahl = b[b.find(":") + 2:-1]
+		for b in file:
+			name = b[:b.find(":")]
+			zahl = b[b.find(":") + 2:-1]
 
-				if name not in nown:
+			if name not in nown:
 
-					for c in files:
-						c = dir + "/" + c
-						findf = open(c, "r").readlines()
-						lastzahl = zahl
+				for c in files:
+					c = dir + "/" + c
+					findf = open(c, "r").readlines()
+					lastzahl = zahl
 
-						for d in findf:
-							name2 = d[:d.find(":")]
-							zahl2 = d[d.find(":") + 2:-1]
+					for d in findf:
+						name2 = d[:d.find(":")]
+						zahl2 = d[d.find(":") + 2:-1]
 
-							if name == name2:
+						if name == name2:
+							raw.append(b[:-1] + " date: " + a[8:-5])
+							raw.append(d[:-1] + " date: " + c[8:-5])
 
-#								print b[:-1] + " date: " + a[8:-5]
-#								print d[:-1] + " date: " + c[8:-5]
-								raw.append(b[:-1] + " date: " + a[8:-5])
-								raw.append(d[:-1] + " date: " + c[8:-5])
-
-
-			nown.append(name)
-			return raw
+		nown.append(name)
+		return raw
