f35e341a996ec859991d3c12aea5cda7b4267a98
patrickzyolo <patrick@kappa-mm.de>
2014-09-12 17:50:28 +0200

    check in

---
+++ spon/conf.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+dir =  "./words/"
+
+text = ["news-spon.txt", "news-welt.txt", "news-zeit.txt", 
+		"news-stern.txt", "news-faz.txt", "news-ntv.txt", 
+		"news-tagesspiegel.txt", "news-sueddeutsche.txt"]
+
