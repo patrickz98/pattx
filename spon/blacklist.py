e0c3c5c35d7cb0deeab81d80dc81caf35e412929
patrickzyolo <patrick@kappa-mm.de>
2014-10-05 14:14:23 +0200

    check in

--- spon/blacklist.py
+++ spon/blacklist.py
@@ -44,7 +44,8 @@ bad = ["um", "an", "von", "in", "den", "haelt", "sicher", "tritt", "essen",
        "erhoeht", "warnt", "Nord", "ruck", "Profi", "ersten", "Fussball:",
        "liess", "ausser", "erneut", "feiert", "spielt", "Moto", "euer",
        "Star", "Neues", "bleibt", "bitten", "fordert", "West", "dass",
-       "weitere", "siegt", "achte", "Gross", "Sued",
+       "weitere", "siegt", "achte", "Gross", "Sued", "verlaesst",
+       "Zustand", "sollen", "lame", "faellt",
        "Deutsche", "Bund"]
 
 exception = ["EU", "Eu", "USA", "US", "BND", "IS", "Tod", "CIA", "NSA", "ARD", "ZDF",
