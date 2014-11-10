f91daffdf43c6dc6a42a674d9a1d2394bc960cb1
patrickzyolo <patrick@kappa-mm.de>
2014-11-10 16:46:32 +0100

    check in

---
+++ java/hallo.java
@@ -0,0 +1,10 @@
+import java.applet.*;
+import java.awt.*;
+
+public class hallo extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawString("Welcome in Java Applet.",40,20);
+	}
+}
---
+++ java/shell.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class shell
+{
+	public static void main(String args[]) throws Exception
+	{
+        Console cons = System.console();
+		cons.printf("Shell: ");
+		String eingabe = cons.readLine();
+		System.out.print(eingabe + "\n");
+
+        Runtime r = Runtime.getRuntime();
+		Process p = r.exec(eingabe);
+		p.waitFor();
+		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line = "";
+
+		while ((line = b.readLine()) != null) 
+		{
+			System.out.println(line);
+		}
+
+		b.close();
+	}
+}
