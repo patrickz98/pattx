1a8e67132122aa24363a1ea4954412bfbdb74aab
patrickzyolo <patrick@kappa-mm.de>
2014-12-11 20:24:37 +0100

    check in

--- java/FileReceiver.java
+++ java/FileReceiver.java
@@ -8,59 +8,75 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 
-public class FileReceiver {
+public class FileReceiver 
+{
+	public static void main(String[] args) 
+	{
+		FileReceiver nioServer = new FileReceiver();
+		SocketChannel socketChannel = nioServer.createServerSocketChannel();
+		nioServer.readFileFromSocket(socketChannel);
+	}
 
-public static void main(String[] args) {
-FileReceiver nioServer = new FileReceiver();
-SocketChannel socketChannel = nioServer.createServerSocketChannel();
-nioServer.readFileFromSocket(socketChannel);
-}
+	public SocketChannel createServerSocketChannel() 
+	{
+		ServerSocketChannel serverSocketChannel = null;
+		SocketChannel socketChannel = null;
+		
+		try 
+		{
+			serverSocketChannel = ServerSocketChannel.open();
+			serverSocketChannel.socket().bind(new InetSocketAddress(9999));
+			socketChannel = serverSocketChannel.accept();
+			System.out.println("Connection established...." + socketChannel.getRemoteAddress());
 
-public SocketChannel createServerSocketChannel() {
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 
-ServerSocketChannel serverSocketChannel = null;
-SocketChannel socketChannel = null;
-try {
-serverSocketChannel = ServerSocketChannel.open();
-serverSocketChannel.socket().bind(new InetSocketAddress(9999));
-socketChannel = serverSocketChannel.accept();
-System.out.println("Connection established...." + socketChannel.getRemoteAddress());
-
-} catch (IOException e) {
-e.printStackTrace();
-}
-
-return socketChannel;
-}
+		return socketChannel;
+	}
 
 /**
 * Reads the bytes from socket and writes to file
 *
 * @param socketChannel
 */
-public void readFileFromSocket(SocketChannel socketChannel) {
-RandomAccessFile aFile = null;
-try {
-aFile = new RandomAccessFile("Video.avi", "rw");
-ByteBuffer buffer = ByteBuffer.allocate(1024);
-FileChannel fileChannel = aFile.getChannel();
-while (socketChannel.read(buffer) > 0) {
-buffer.flip();
-fileChannel.write(buffer);
-buffer.clear();
-}
-Thread.sleep(1000);
-fileChannel.close();
-System.out.println("End of file reached..Closing channel");
-socketChannel.close();
+	public void readFileFromSocket(SocketChannel socketChannel) 
+	{
+		RandomAccessFile aFile = null;
+	
+		try 
+		{
+			aFile = new RandomAccessFile("Video.avi", "rw");
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			FileChannel fileChannel = aFile.getChannel();
+		
+			while (socketChannel.read(buffer) > 0) 
+			{
+				buffer.flip();
+				fileChannel.write(buffer);
+				buffer.clear();
+			}
 
-} catch (FileNotFoundException e) {
-e.printStackTrace();
-} catch (IOException e) {
-e.printStackTrace();
-} catch (InterruptedException e) {
-e.printStackTrace();
-}
+			Thread.sleep(1000);
+			fileChannel.close();
+			System.out.println("End of file reached..Closing channel");
+			socketChannel.close();
 
-}
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
--- java/FileSender.java
+++ java/FileSender.java
@@ -10,63 +10,73 @@ import java.nio.channels.SocketChannel;
 
 public class FileSender 
 {
-	public static void main(String[] args) {
-	FileSender nioClient = new FileSender();
-	SocketChannel socketChannel = nioClient.createChannel();
-	nioClient.sendFile(socketChannel);
-}
+	public static void main(String[] args) 
+	{
+		FileSender nioClient = new FileSender();
+		SocketChannel socketChannel = nioClient.createChannel();
+		nioClient.sendFile(socketChannel);
+	}
 
 /**
 * Establishes a socket channel connection
 *
 * @return
 */
-public SocketChannel createChannel() 
-{
+	public SocketChannel createChannel() 
+	{
 
-	SocketChannel socketChannel = null;
+		SocketChannel socketChannel = null;
 	
-	try 
-	{
-		socketChannel = SocketChannel.open();
-		SocketAddress socketAddress = new InetSocketAddress("localhost", 9999);
-		socketChannel.connect(socketAddress);
-		System.out.println("Connected..Now sending the file");
-	} 
-	catch (IOException e) 
-	{
-		e.printStackTrace();
+		try 
+		{
+			socketChannel = SocketChannel.open();
+			SocketAddress socketAddress = new InetSocketAddress("localhost", 9999);
+			socketChannel.connect(socketAddress);
+			System.out.println("Connected..Now sending the file");
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+
+		return socketChannel;
 	}
 
-	return socketChannel;
-}
 
+	public void sendFile(SocketChannel socketChannel) 
+	{
+		RandomAccessFile aFile = null;
+		try 
+		{
+			File file = new File("ChinAmerika-1.mkv");
+			aFile = new RandomAccessFile(file, "r");
+			FileChannel inChannel = aFile.getChannel();
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-public void sendFile(SocketChannel socketChannel) 
-{
-RandomAccessFile aFile = null;
-try {
-File file = new File("ChinAmerika-1.mkv");
-aFile = new RandomAccessFile(file, "r");
-FileChannel inChannel = aFile.getChannel();
-ByteBuffer buffer = ByteBuffer.allocate(1024);
-while (inChannel.read(buffer) > 0) {
-buffer.flip();
-socketChannel.write(buffer);
-buffer.clear();
-}
-Thread.sleep(1000);
-System.out.println("End of file reached..");
-socketChannel.close();
-aFile.close();
-} catch (FileNotFoundException e) {
-e.printStackTrace();
-} catch (IOException e) {
-e.printStackTrace();
-} catch (InterruptedException e) {
-e.printStackTrace();
-}
+			while (inChannel.read(buffer) > 0) 
+			{
+				buffer.flip();
+				socketChannel.write(buffer);
+				buffer.clear();
+			}
 
-}
+			Thread.sleep(1000);
+			System.out.println("End of file reached..");
+			socketChannel.close();
+			aFile.close();
 
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
