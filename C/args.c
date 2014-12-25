833d83db2fc24bb009f1fb2f078d759187c91d13
patrickzyolo <patrick@kappa-mm.de>
2014-12-26 00:22:20 +0100

    check in

---
+++ C/args.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf( "args --> %i \n", argc );
+	for (int x = 0; argc > x; x++)
+	{
+		printf( "you say %s \n", argv[x] );
+    }
+}
--- C/help.py
+++ C/help.py
@@ -1,10 +1,25 @@
 import os
+import sys
 
-dir =  os.listdir(".")
-data = {}
+def clean():
+	dir =  os.listdir(".")
 
-for x in dir:
-	for y in dir:
-		if x == y[:-2]:
-			print "clean --> " + x
-			os.remove(x)
+	for x in dir:
+		for y in dir:
+			if x == y[:-2]:
+				print "clean --> " + x
+				os.remove(x)
+
+def list():
+	dir =  os.listdir(".")
+
+	for x in dir:
+			if ".c" in x:
+				print "\tgcc " + x + " -o " + x[:-2]
+
+if sys.argv[1] == "clean":
+	clean()
+elif sys.argv[1] == "list":
+	list()
+else:
+	print "NOOO"
