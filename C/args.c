72953fa7991585fd1fc9ca9a083546a6204ffbf1
patrickzyolo <patrick@kappa-mm.de>
2014-12-31 18:46:29 +0100

    check in

--- C/args.c
+++ C/args.c
@@ -2,9 +2,9 @@
 
 int main(int argc, char *argv[])
 {
-    printf( "args --> %i \n", argc );
+	printf( "args --> %i \n", argc );
 	for (int x = 0; argc > x; x++)
 	{
 		printf( "you say %s \n", argv[x] );
-    }
+	}
 }
--- C/string.c
+++ C/string.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-
 int main()
 {
-	int i = 0;
 	char satz[10];
+	char test[20];
 
-	printf("\nBitte einen Satz eingeben: ");
-    scanf("%9c", satz);
-    fflush(stdin);
-	
-	printf("Eingabe: %s\n", satz);
-	printf("Der Satz besteht aus %lu Zeichen\n", strlen(satz));
+	printf("\n## 1 ## Bitte einen Satz eingeben: ");
+	scanf("%9s", satz);
+//  fgets(satz, 9, stdin);
+	fflush(stdin);
 
-	while (satz[i] != '\0')
-	{
-		printf("%c", satz[i]);
-		i++;
-	}
-	printf("\nSatz[5] enthält eine null\n");
-	satz[5] = '\0';
+	printf("## 1 ## Eingabe: %s\n", satz);
+	printf("## 1 ## besteht aus %lu Zeichen\n", strlen(satz));
+	printf("\n## 2 ## Bitte einen Satz eingeben: ");
+	scanf("%19s", test);
+//  fgets(test, 19, stdin);
 
-	printf("Der Satz besteht aus %lu Zeichen\n\n", strlen(satz));
+	fflush(stdin);
+
+	printf("## 2 ## Eingabe: %s\n", test);
+	printf("## 2 ## besteht aus %lu Zeichen\n", strlen(test));
 
 	return 0;
 }
