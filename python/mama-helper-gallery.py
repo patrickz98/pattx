0ff7c0abda25218279f4bb24ff7227478fb17e2c
patrickzyolo <patrick@kappa-mm.de>
2015-01-03 16:17:32 +0100

    check in

---
+++ python/mama-helper-gallery.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+from urllib2 import Request, urlopen, URLError
+
+def download(url):
+	request = Request(url)
+	html = 0
+	dir = "./gallery/"
+
+	try:
+		response = urlopen(request)
+		html = response.read()
+		sys.stdout.write("Download: " + url.split("/")[len(url.split("/")) - 1].replace("%20", "-") + "\n") 
+	except URLError, e:
+		print 'Error:', e
+
+	try:
+ 		os.mkdir(dir)
+	except:
+		pass
+	
+	img = open(dir + url.split("/")[len(url.split("/")) - 1].replace("%20", "-"), 'wb')
+	img.write(html)
+	img.close()
+
+def get(url, count):
+	ur_url = url.split("://")[1].split("/", 1)[0]
+
+	for n in range(0, count):
+		request = Request(url + str(n))
+
+		try:
+			response = urlopen(request)
+			html = response.read()
+		except URLError, e:
+			sys.stderr.write('Error:', e)
+
+		find = re.findall("<img.*?src=\"(.*?)\".*?>", html)
+
+		for x in find:
+			download("http://" + ur_url + x.replace(" ", "%20").replace("1__", ""))
+
+url = input('Enter URL: ')
+size = input('Enter pictures size: ')
+
+get(url.replace("https", "http"), size - 1)
--- python/mama-helper.py
+++ python/mama-helper.py
@@ -5,37 +5,40 @@ import re
 import sys
 from urllib2 import Request, urlopen, URLError
 
-def download(url):
-	request = Request(url)
+def download(url, file):
+	request = Request(url + "/images/" + file)
 	html = 0
 
 	try:
 		response = urlopen(request)
 		html = response.read()
-		sys.stdout.write("Download: " + url.split("/")[len(url.split("/")) - 1].replace("%20", "-") + "\n") 
-	except URLError, e:
-		print 'Error:', e
+		sys.stdout.write("Download: " + file.replace("%20", "-") + "\n") 
 
-	img = open(url.split("/")[len(url.split("/")) - 1].replace("%20", "-"), 'wb')
+	except URLError, e:
+		sys.stdout.write('Error:', e)
+
+	img = open(file.replace("%20", "-"), 'wb')
 	img.write(html)
 	img.close()
 
-def get(url, count):
+def get(url):
 	ur_url = url.split("://")[1].split("/", 1)[0]
 
-	for n in range(0, count):
-		request = Request(url + str(n))
+	request = Request(url)
 
-		try:
-			response = urlopen(request)
-			html = response.read()
-		except URLError, e:
-			sys.stderr.write('Error:', e)
+	try:
+		response = urlopen(request)
+		html = response.read()
+	except URLError, e:
+		sys.stderr.write('Error:', e)
 
-		find = re.findall("<img.*?src=\"(.*?)\".*?>", html)
-		download("http://" + ur_url + find[0].replace(" ", "%20").replace("1__", ""))
+	find = re.findall("<img.*?src=\"(.*?)\".*?>", html)
+
+	for x in find:
+		if "images" in x:
+			file = x.split("/")[len(x.split("/")) - 1].replace(" ", "%20").replace("1__", "")
+			download("http://" + ur_url, file)
 
 url = input('Enter URL: ')
-size = input('Enter pictures size: ')
 
-get(url.replace("https", "http"), size - 1)
+get(url.replace("https", "http"))
