9a8ddb637ff36b8bd731fd72612fdf5d7b0e51ac
patrickz98 <patrick@kappa-mm.de>
2016-05-20 12:58:57 +0200

    Check in.

--- Spider/Elastic.php
+++ Spider/Elastic.php
@@ -1,4 +1,9 @@
 <?php
+function brockePa()
+{
+    echo "test987";
+    return "test";
+}
 
 function putData($json, $server, $index, $type, $id = false)
 {
@@ -45,4 +50,53 @@ function postData($json, $server, $index, $type)
     curl_close($chlead);
 }
 
+// function getDataById($server, $index, $type, $id)
+function brocke()
+{
+    // $url = "http://$server:9200/$index/$type/$id";
+    //
+    // $chlead = curl_init();
+    //
+    // curl_setopt($chlead, CURLOPT_URL, $url);
+    // curl_setopt($chlead, CURLOPT_CUSTOMREQUEST, "GET");
+    // curl_setopt($chlead, CURLOPT_RETURNTRANSFER, 1);
+    //
+    // $output = curl_exec($chlead);
+    // curl_close($chlead);
+
+    // return $output;
+    return "end";
+}
+
+function getDataByUuid($server, $index, $type, $uuid)
+{
+    $url = "http://$server:9200/$index/$type/_search?q=uuid:$uuid";
+
+    $chlead = curl_init();
+
+    curl_setopt($chlead, CURLOPT_URL, $url);
+    curl_setopt($chlead, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($chlead, CURLOPT_RETURNTRANSFER, 1);
+
+    $output = curl_exec($chlead);
+    curl_close($chlead);
+
+    return $output;
+}
+
+function getDataById($server, $index, $type, $id)
+{
+    $url = "http://$server:9200/$index/$type/$id";
+
+    $chlead = curl_init();
+
+    curl_setopt($chlead, CURLOPT_URL, $url);
+    curl_setopt($chlead, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($chlead, CURLOPT_RETURNTRANSFER, 1);
+
+    $output = curl_exec($chlead);
+    curl_close($chlead);
+
+    return $output;
+}
 ?>
