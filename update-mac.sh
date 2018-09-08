3e4daa7b0271561a2dc3e7d52d82410ee18034e9
patrickz98 <patrickz98@users.noreply.github.com>
2018-09-08 12:22:15 +0200

    Check in. 2018-09-08 12:22

--- update-mac.sh
+++ update-mac.sh
@@ -6,13 +6,12 @@ brew upgrade
 brew cleanup
 npm update -g
 npm install -g npm
-sudo port -v selfupdate
-sudo port selfupdate
-sudo port upgrade outdated
-sudo port uninstall inactive
-sudo composer self-update
+#sudo port -v selfupdate
+#sudo port selfupdate
+#sudo port upgrade outdated
+#sudo port uninstall inactive
+#sudo composer self-update
 pip install -U pip
 flutter upgrade
 rustup update stable
 pip3 install --upgrade pip
-
